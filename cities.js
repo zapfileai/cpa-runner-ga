6290e3e8465c250e08fe025fdf354d6727de9aba
Sreehari Annam <sannam@wescodist.com>
2026-07-04 09:49:52 -0400

    batch 6: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,5 @@
 const georgiaCities = [
-  "Suches", "Rossville", "Scottdale", "Naylor", "Trion",
-  "Newington", "Morrow", "Toccoa", "Rydal", "Register",
-  "Townsend", "Tiger", "Powder Springs", "Monticello", "Rockmart",
-  "Taylorsville", "Patterson", "Summerville", "Surrency", "Oglethorpe",
-  "Pavo", "Rutledge", "Ray City", "Royston", "Milledgeville",
-  "Pine Mountain", "Milan", "Port Wentworth", "Stockbridge", "Thomson",
-  "Midland", "Savannah", "Pendergrass", "Reynolds", "Talmo",
-  "Millwood", "Swainsboro", "Richland", "Ranger", "Nicholls",
-  "Mount Airy", "Nahunta", "Midville", "Sharpsburg", "Odum",
-  "Pembroke", "Sylvester", "Mount Zion", "Social Circle", "Metter",
-  "Pooler", "Richmond Hill", "Norcross", "Sylvania", "Quitman",
-  "Riverdale", "Ocilla", "Molena", "Rising Fawn", "Stone Mountain",
-  "Roopville", "Tifton", "Tignall", "Pineview", "Rincon",
-  "Senoia", "Statesboro", "Shiloh", "Statham", "Rentz",
-  "Trenton", "Silver Creek", "Rock Spring", "Newborn", "Montezuma",
-  "Midway", "Pinehurst", "Resaca", "Murrayville", "Sycamore",
+  "Temple", "Rabun Gap", "Moultrie", "Tennille", "Pine Mountain Valley",
 ];
 
 const BATCH_SIZE = 5;
